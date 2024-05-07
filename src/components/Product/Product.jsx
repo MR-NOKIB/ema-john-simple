4da72e202e096b83f0b478da376dbcfc91e0e9fc
MR-NOKIB <nozomuddinnokib03@gmail.com>
2024-05-07 08:41:19 +0100

    retrive and set cart from savedCart (local storage)

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'    
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Product.css'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
@@ -18,7 +18,7 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings}</p>
             </div>
-            <button onClick={ () => handelAddToCart(props.product)} className='btn-cart'>Add To Cart <FontAwesomeIcon icon={faShoppingCart} /></button>
+            <button onClick={() => handelAddToCart(props.product)} className='btn-cart'>Add To Cart <FontAwesomeIcon icon={faShoppingCart} /></button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -17,8 +17,18 @@ const Shop = () => {
 
     useEffect(() => {
         const storedCart = getShoppingCart();
-        console.log(storedCart)
-    }, []);
+        // step 1: get id
+        for (const id in storedCart) {
+            // step 2: get the product using id'
+            const addedProduct = products.find(product => product.id === id);
+
+           if(addedProduct){
+             //step 3: get the quantity of the product
+             const quantity = storedCart[id]
+             addedProduct.quantity = quantity;
+           }
+        }
+    }, [products]);
 
     const handelAddToCart = (product) => {
         const newCart = [...cart, product];
